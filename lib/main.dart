dbd3ac40f17c0b53d895f9d659ea46925bd49f2b
Oscar C. <ocalle374@gmail.com>
2026-06-19 20:49:29 -0700

    added hamburger to left, added phone, license, email icons to right, added background image to center of Header

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'widgets/navbar.dart';
+// import 'package:sticky_headers/sticky_headers.dart';
 
 void main() {
+  //runapp is the entry point of the Flutter application, it runs the MyApp widget which is the root of the widget tree
   runApp(const MyApp());
 }
 
@@ -15,45 +17,55 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       //Body background color ***scaffoldBackgroundColor: Colors.white - suggested color
       theme: ThemeData(
-        primaryColor: const Color(0xFFF97316), // Orange
+        primaryColor: const Color.fromARGB(255, 34, 15, 1), // Orange
         scaffoldBackgroundColor: const Color.fromARGB(255, 233, 134, 13),
       ),
-      home: const HomePage(),
+      home: Navbar(),
+      // home: const HomePage(),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+// class HomePage extends StatelessWidget {
+//   const HomePage({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        surfaceTintColor: Colors.white,
-        elevation: 2,
-        toolbarHeight: 120,
-        centerTitle: true,
-        iconTheme: const IconThemeData(
-          color: Color(0xFF1F2937), // Dark gray menu icon
-        ),
-        title: Image.asset(
-          'assets/images/logo.webp',
-          height: 100,
-        ),
-      ),
-      drawer: const Navbar(),
-      body: const Center(
-        child: Text(
-          'Home Pagedsadasdsaddsadsadsadasdasdsadsadsaas',
-          style: TextStyle(
-            color: Color(0xFF1F2937),
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
-          ),
-        ),  
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         backgroundColor: Colors.white,
+//         surfaceTintColor: Colors.white,
+//         elevation: 2,
+//         toolbarHeight: 120,
+//         centerTitle: true,
+//         iconTheme: const IconThemeData(
+//           color: Color(0xFF1F2937), // Dark gray menu icon
+//         ),
+//         title: Image.asset(
+//           'assets/images/logo.webp',
+//           height: 100,
+//         ),
+//       ),
+//       drawer: const Navbar(),
+//       body: ListView.builder(
+//         itemCount: 20,
+//         itemBuilder: (context, index) => StickyHeaderBuilder(
+//           header: Container(
+//             width: double.infinity,
+//             color: Colors.red,
+//             padding: const EdgeInsets.all(16),
+//             child: Text('Sticky Header $index'),
+//           ),
+//           content: Image.network(
+//             'https://unsplash.com/photos/a-white-fire-hydrant-sitting-in-front-of-a-garage-door-0ujK6DGzWBE',
+//             fit: BoxFit.cover,
+//             width: double.infinity,
+//             height: 200,
+//           ), builder: (BuildContext context, double stuckAmount) { 
+            
+//            },
+//         ),
+//       )
+//       );
+//   }
+// }
--- lib/widgets/navbar.dart
+++ lib/widgets/navbar.dart
@@ -3,75 +3,147 @@ import 'package:flutter/material.dart';
 
 class Navbar extends StatelessWidget {
   const Navbar({super.key});
-
+  
   @override
   Widget build(BuildContext context) {
-    return Drawer(
-      child: ListView(
-        padding: EdgeInsets.zero,
-        children: [
-          const DrawerHeader(
-            decoration: BoxDecoration(
-              color: Colors.blue,
-            ),
-            child: Text(
-              'Garage Door Hero',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 24,
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+           SliverAppBar(
+              title: const Text('Garage Doors Hero anddsassa gate'),
+            //You want the Home,servies, and others here and then when I scroll down they will go to the side of the logo and then when I scroll down more they will disappear
+              leading: IconButton(
+                icon: Icon(Icons.menu),
+                onPressed: () {},
               ),
+              actions: [
+                IconButton(
+                  icon: const Icon(
+                    Icons.phone,
+                  color: Colors.black,
+                  size: 30.0,  
+                    ),
+                  onPressed: () {
+                    print('Phone icon pressed');
+                    // 951-498-7958;
+                  },
+                ),
+                IconButton(
+                  icon: const Icon(
+                    Icons.email,
+                    color: Colors.black,
+                    size: 30.0,
+                  ),
+                  onPressed: () {
+                    print('garagedoorshero@gmail.com');
+                    // email address
+                  },
+                ),
+                IconButton(
+                  icon: const Icon(
+                    Icons.card_membership,
+                    color: Colors.black,
+                    size: 30.0,
+                  ),
+                  onPressed: () {
+                    print('#1066860');
+                    // email address
+                  },
+                ),
+              ],
+            floating: true, //Hides the expanded height as I scroll down
+            pinned: true,
+            expandedHeight: 500.0,
+            flexibleSpace: FlexibleSpaceBar(
+              //Can change the background to an image, video, or even a widget
+              background: Image.network(
+                'https://media.istockphoto.com/id/172474435/photo/small-family-poses-in-front-of-new-home.jpg?s=2048x2048&w=is&k=20&c=BZ1c34VyDhur_obsHggmbm7SHg3-DzZIyuqRaGJl_2M=',
+                fit: BoxFit.contain,//Can change to where I can have it cover the entire space or fit the entire space
+              ),
+            ), 
+            backgroundColor: const Color.fromARGB(255, 242, 241, 241),
             ),
-          ),
-
-          ListTile(
-            leading: const Icon(Icons.home),
-            title: const Text('Home'),
-            onTap: () {
-              Navigator.pop(context);
-            },
-          ),
-
-          ListTile(
-            leading: const Icon(Icons.build),
-            title: const Text('Services'),
-            onTap: () {
-              Navigator.pop(context);
-            },
-          ),
-
-          ListTile(
-            leading: const Icon(Icons.info),
-            title: const Text('About Us'),
-            onTap: () {
-              Navigator.pop(context);
-            },
-          ),
-
-          ListTile(
-            leading: const Icon(Icons.contact_mail),
-            title: const Text('Contact Us'),
-            onTap: () {
-              Navigator.pop(context);
-            },
-          ),
-
-          ListTile(
-            leading: const Icon(Icons.location_on),
-            title: const Text('Location'),
-            onTap: () {
-              Navigator.pop(context);
-            },
-          ),
-
-          ListTile(
-            leading: const Icon(Icons.article),
-            title: const Text('Blog'),
-            onTap: () {
-              Navigator.pop(context);
-            },
+          SliverList(
+delegate: SliverChildBuilderDelegate((context, index) => ListTile(
+  title: Text('Item #$index'),
+),
+childCount: 30,
+),
           ),
         ],
       ),
     );
   }
-}
+  }
+
+  // }
+
+//   @override
+//   Widget build(BuildContext context) => Drawer(
+//       child: ListView(
+//         padding: EdgeInsets.zero,
+//         children: [
+//           const DrawerHeader(
+//             decoration: BoxDecoration(
+//               color: Colors.blue,
+//             ),
+//             child: Text(
+//               'Garage Door Hero',
+//               style: TextStyle(
+//                 color: Colors.white,
+//                 fontSize: 24,
+//               ),
+//             ),
+//           ),
+
+//           ListTile(
+//             leading: const Icon(Icons.home),
+//             title: const Text('Home'),
+//             onTap: () {
+//               Navigator.pop(context);
+//             },
+//           ),
+
+//           ListTile(
+//             leading: const Icon(Icons.build),
+//             title: const Text('Services'),
+//             onTap: () {
+//               Navigator.pop(context);
+//             },
+//           ),
+
+//           ListTile(
+//             leading: const Icon(Icons.info),
+//             title: const Text('About Us'),
+//             onTap: () {
+//               Navigator.pop(context);
+//             },
+//           ),
+
+//           ListTile(
+//             leading: const Icon(Icons.contact_mail),
+//             title: const Text('Contact Us'),
+//             onTap: () {
+//               Navigator.pop(context);
+//             },
+//           ),
+
+//           ListTile(
+//             leading: const Icon(Icons.location_on),
+//             title: const Text('Location'),
+//             onTap: () {
+//               Navigator.pop(context);
+//             },
+//           ),
+
+//           ListTile(
+//             leading: const Icon(Icons.article),
+//             title: const Text('Blog'),
+//             onTap: () {
+//               Navigator.pop(context);
+//             },
+//           ),
+//         ],
+//       ),
+//     );
+// }
